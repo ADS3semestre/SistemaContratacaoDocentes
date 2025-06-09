4b0e3b5725e1d33214f5d68e36bf56235c9e9b4a
Larissa Muller <165028464+LariMuller@users.noreply.github.com>
2025-06-09 20:13:44 -0300

    Adding OS verification to CSVController

--- src/control/CSVController.java
+++ src/control/CSVController.java
@@ -3,22 +3,36 @@ package control;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.LocalTime;
 
 import com.destny.model.ListaLib;
 import model.Disciplinas;
 
 public class CSVController {
-    public static ListaLib<Disciplinas> getDisciplinas() {
+
+    public CSVController(){
+        super();
+    }
+
+    String os = getOS(); //Pega o SO para abrir o arquivo corretamente
+
+    public ListaLib<Disciplinas> getDisciplinas() {
         ListaLib<Disciplinas> disciplinas = new ListaLib<>();
         BufferedReader reader = null;
         String line = "";
+        String fileName = "";
+
+        if (os.contains("Windows")) {
+            fileName = ".\\files\\disciplinas.csv";
+        }
+        if (os.contains("Linux")){
+            fileName = ".//files//disciplinas.csv";
+        }
 
         try {
-            reader = new BufferedReader(new FileReader("./files/disciplinas.csv"));
-            reader.readLine();
-            while ((line = reader.readLine()) != null) {
+            reader = new BufferedReader(new FileReader(fileName));
+            reader.readLine(); //Pula o cabeçalho do arquivo
+            while ((line = reader.readLine()) != null) { //
                 String[] row = line.split(",");
                 Disciplinas disciplina = new Disciplinas(row[0], row[1], row[2], LocalTime.parse(row[3]), Integer.parseInt(row[4]), row[5]);
                 disciplinas.addLast(disciplina);
@@ -34,4 +48,9 @@ public class CSVController {
         }
         return disciplinas;
     }
+
+    private static String getOS() {
+		String os = System.getProperty("os.name");
+		return os;
+	}
 }
